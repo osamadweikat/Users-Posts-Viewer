84ae0d223825e43358c3d3068f332c2322fdd9ea
osamadweikat <osamadweikat83@gmail.com>
2025-08-05 17:32:28 +0300

    feat: add script-axios.js and update index.html to use Axios instead of Fetch/XMLHttpRequest

---
+++ script-axios.js
@@ -0,0 +1,64 @@
+function getUsers() {
+  return new Promise((resolve, reject) => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        let users = response.data;
+        document.getElementById("users").innerHTML = "";
+        for (user of users) {
+          let content = `
+                <div id="user" onclick="userClicked(${user.id}, this)">
+                    <h3 class="name">${user.name}</h3>
+                    <h3 class="email">${user.email}</h3>
+                </div>
+                `;
+          document.getElementById("users").innerHTML += content;
+        }
+        resolve();
+      })
+      .catch((error) => {
+        alert(error);
+        reject(error);
+      });
+  });
+}
+
+function getPosts(userId) {
+  axios
+    .get("https://jsonplaceholder.typicode.com/posts?userId=" + userId)
+    .then((response) => {
+      let posts = response.data;
+      document.getElementById("posts").innerHTML = "";
+      for (post of posts) {
+        let content = `
+                <div id="post">
+                    <h3 class="title">${post.title}</h3>
+                    <h4 class="body">
+                        ${post.body}
+                    </h4>
+                </div>
+                `;
+        document.getElementById("posts").innerHTML += content;
+      }
+    })
+    .catch((error) => {
+      alert(error);
+    });
+}
+
+function userClicked(id, el) {
+  getPosts(id);
+  let selectedElements = document.getElementsByClassName("selected");
+  for (element of selectedElements) {
+    element.classList.remove("selected");
+  }
+  el.classList.add("selected");
+}
+
+getUsers()
+  .then(() => {
+    getPosts(1);
+  })
+  .catch((error) => {
+    alert(error);
+  });
